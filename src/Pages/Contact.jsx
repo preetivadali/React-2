970c1dd5eb49452e49d6a74c4f08a04022671759
preetivadali <preeti.abhiram@gmail.com>
2024-11-21 23:26:35 +0100

    created components folder and created navbar and footer files, and created pages folder and created home, about and contact files

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Feel free to reach out via email or phone!</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the homepage of our React app.</p>
+    </div>
+  );
+}
+
+export default Home;
